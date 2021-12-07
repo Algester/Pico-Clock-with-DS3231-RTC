2229ac28f257ebfdcbf710f029ab18fbbfa62dce
Algester <ragnarok_mmorpg@hotmail.com>
2021-12-07 19:20:22 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -14,7 +14,6 @@ I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
 
 i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
-#i2c = I2C(0, sda=machine.Pin(4), scl=machine.Pin(5))
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
 
