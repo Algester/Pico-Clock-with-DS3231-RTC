b0cb2724ecf899fbd1309b9a985034bae71803fd
Algester <ragnarok_mmorpg@hotmail.com>
2021-12-07 17:02:15 +0800

    Create main_test.py
    debugging why 
    Traceback (most recent call last):
      File "<stdin>", line 35, in <module>
      File "/lib/lcd_api.py", line 124, in move_to
      File "/lib/pico_i2c_lcd.py", line 63, in hal_write_command
    OSError: [Errno 5] EIO
    
    happens

---
+++ main_test.py
@@ -0,0 +1,46 @@
+from lib import RTC_DS3231
+from lib import am2320
+import time
+
+import utime
+
+import machine
+from machine import I2C
+from lcd_api import LcdApi
+from pico_i2c_lcd import I2cLcd
+
+I2C_ADDR     = 0x27
+I2C_NUM_ROWS = 2
+I2C_NUM_COLS = 16
+
+i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+#i2c = I2C(0, sda=machine.Pin(4), scl=machine.Pin(5))
+lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+
+#RTC
+rtc = RTC_DS3231.RTC()
+
+#Humidity and Temperature
+sensor_i2c = I2C(0, sda=machine.Pin(4), scl=machine.Pin(5))
+sensor = am2320.AM2320(sensor_i2c)
+
+#Weekday is start at Saturday with x01
+#                     sec\min\hou\wee\day\mon\yea
+#rtc.DS3231_SetTime(b'\x50\x40\x16\x04\x07\x12\x21')
+#remove comment to set time. Do this only once otherwise time will be set everytime the code is executed.
+
+while True:
+    t = rtc.DS3231_ReadTime(1)
+    #read RTC and receive data in Mode 1 (see /my_lib/RTC_DS3231.py)
+    lcd.move_to(0,0)
+    lcd.putstr(t)
+    time.sleep(1)
+    if machine.Pin(20, machine.Pin.OUT):
+        sensor.measure()
+        lcd.clear()
+        lcd.move_to(0,0)
+        lcd.putstr(sensor.temperature())
+        lcd.putstr(sensor.humidity())
+        time.sleep_ms(30)
+
+
