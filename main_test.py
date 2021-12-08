62c0f20fd0c72d7eb3b7e1722233a60de2d0833c
Algester <ragnarok_mmorpg@hotmail.com>
2021-12-08 08:05:50 +0800

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -18,11 +18,11 @@ lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
 
 
 #RTC
-rtc_i2c= I2C(1, sda=machine.Pin(2), scl=machine.Pin(3), freq=100000)
+rtc_i2c= I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=100000)
 rtc = RTC_DS3231.RTC(rtc_i2c)
 
 #Humidity and Temperature
-sensor_i2c = I2C(0, sda=machine.Pin(4), scl=machine.Pin(5))
+sensor_i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1))
 sensor = am2320.AM2320(sensor_i2c)
 
 #if possible attempt to use GPIO 20-22 buttons to configure time rather than set them on the PC first
@@ -31,18 +31,20 @@ sensor = am2320.AM2320(sensor_i2c)
 #rtc.DS3231_SetTime(b'\x50\x40\x16\x04\x07\x12\x21')
 #remove comment to set time. Do this only once otherwise time will be set everytime the code is executed.
 
+pin = machine.Pin(20, machine.Pin.IN)
+temp = sensor.temperatur()
+humi = sensor.humidity()
+
 while True:
     t = rtc.DS3231_ReadTime(1)
-    temp = sensor.temperatur()
-    humi = sensor.humidity()
     #read RTC and receive data in Mode 1 (see /my_lib/RTC_DS3231.py)
     lcd.move_to(0,0)
     lcd.putstr(t)
     time.sleep(1)
     #attempt to use maker pi's built in buttons
-    if machine.Pin(20, machine.Pin.OUT):
-        sensor.measure()
+    if pin.value() == 0:
         lcd.clear()
+        sensor.measure()
         lcd.move_to(0,0)
         lcd.putstr(temp)
         lcd.move_to(1,0)
